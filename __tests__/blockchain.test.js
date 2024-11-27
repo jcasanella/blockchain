945f93b4abd2db6b0d64e8d94127bc653c287457
Jordi Casanella <jordikiter@hotmail.es>
2024-11-27 23:52:29 +0000

    Add test for addTransaction

--- __tests__/blockchain.test.js
+++ __tests__/blockchain.test.js
@@ -43,4 +43,23 @@ describe('Blockchain', () => {
             {index: 3, nonce: 2899, hash: '99889HBAIUSBDF', previousBlockHash: 'UINIUN90ANSFD', timestamp: expect.anything()}
         );
     });
+
+    test('it should create a new transaction', () => {
+        const blockchain = new Blockchain();
+
+        blockchain.createNewBlock(2389,'OINA90SDNF90N','90ANSD9F0N9009N');
+        const ret = blockchain.createNewTransaction({ amount: 500, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'});
+        const block = blockchain.getLastBlock();
+
+        // Validate block stored & pending transactions
+        expect(block).toMatchObject(
+            {index: 1, nonce: 2389, hash: '90ANSD9F0N9009N', previousBlockHash: 'OINA90SDNF90N', timestamp: expect.anything()}
+        );
+        expect(blockchain.chain).toHaveLength(1);
+        expect(blockchain.pendingTransactions).toHaveLength(1);
+        expect(blockchain.pendingTransactions[0]).toMatchObject({ amount: 500, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'});
+
+        // Validate where will be the transaction stored
+        expect(ret).toBe(2);
+    })
 })
