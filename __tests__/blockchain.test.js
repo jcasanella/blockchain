b1186d5cb2d3f013c3068509f5085c4292c6ab90
Jordi Casanella <jordikiter@hotmail.es>
2024-12-03 00:35:40 +0000

    Test mining multiple blocks

--- __tests__/blockchain.test.js
+++ __tests__/blockchain.test.js
@@ -66,18 +66,37 @@ describe('Blockchain', () => {
     test('it should mine a block', async () => {
         const blockchain = new Blockchain();
 
+        // Starting block
         blockchain.createNewBlock(2389,'OINA90SDNF90N','90ANSD9F0N9009N');
 
-        blockchain.createNewTransaction({ amount: 500, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'});
+        // Create transaction
+        blockchain.createNewTransaction({ amount: 500, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9' });
         expect(blockchain.pendingTransactions).toHaveLength(1);
 
+        // mine block
         blockchain.createNewBlock(548764,'AKMC875E6S1RS9','WPLS214R7T6SJ3G2');
-
         expect(blockchain.chain).toHaveLength(2);
         expect(blockchain.chain[0].transactions).toHaveLength(0);
 
         expect(blockchain.chain[1].transactions).toHaveLength(1);
         expect(blockchain.chain[1].transactions).toMatchObject([{ amount: 500, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'}]);
+
+        expect(blockchain.pendingTransactions).toHaveLength(0);
+
+        // Create transactions
+        blockchain.createNewTransaction({ amount: 50, sender: 'ALEXHT845SJ5TKCJ2', recipient: 'JENN5BG5DF6HT8NG9' });
+        blockchain.createNewTransaction({ amount: 200, sender: 'ALEXHT845SJ5TKCJ2', recipient: 'JENN5BG5DF6HT8NG9' });
+        blockchain.createNewTransaction({ amount: 300, sender: 'ALEXHT845SJ5TKCJ2', recipient: 'JENN5BG5DF6HT8NG9' });
+        expect(blockchain.pendingTransactions).toHaveLength(3);
+
+        // Mine block
+        blockchain.createNewBlock(5632,'RTG64BG12RW','ASDF567JMNFD34');
+        expect(blockchain.chain[2].transactions).toHaveLength(3);
+        expect(blockchain.chain[2].transactions).toMatchObject([
+            { amount: 50, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'},
+            { amount: 200, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'},
+            { amount: 300, sender: 'ALEXHT845SJ5TKCJ2',  recipient: 'JENN5BG5DF6HT8NG9'}
+        ]);
 
         expect(blockchain.pendingTransactions).toHaveLength(0);
     })
